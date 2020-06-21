e3e13e830bc3f989d2ff96b7d6df59fba7dfe978
Noam Domovich <ndomovich@gmail.com>
2020-06-21 10:58:10 +0300

    backend-front uppoad

---
+++ django-rest-recipes/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\ndomo\allprojects\pycharmprojects\django-rest-recipes\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
